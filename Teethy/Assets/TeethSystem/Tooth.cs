4cfa18cb06a97fbad4a654b187e65e15b5e030db
Polylyly <polybius404@gmail.com>
2025-03-13 02:00:14 +0200

    Almost done
    UV (or something) still needs to be fixed

--- Teethy/Assets/TeethSystem/Tooth.cs
+++ Teethy/Assets/TeethSystem/Tooth.cs
@@ -11,6 +11,8 @@ public class Tooth : MonoBehaviour
     public float toothLength, maxDistanceBetweenInitialPoints, offset, offsetAtNull;
     public Color toothColour;
     public GameObject holeDecal;
+    public SkinnedMeshRenderer avatarMesh;
+    public int[] materialNumbers;
     [SerializeField]
     float distanceFromInitial, highestDistance;
     bool bite = false;
@@ -26,7 +28,10 @@ public class Tooth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (highestDistance == 0) GetComponent<Renderer>().material.SetVector("_Offset", new Vector2(0, offsetAtNull));
+        if (highestDistance == 0)
+        {
+            foreach (int x in materialNumbers) avatarMesh.materials[x].SetVector("_Offset", new Vector2(0, offsetAtNull));
+        }
         if (bite)
         {
             distanceFromInitial = Mathf.Clamp((Vector3.Dot(toothTip.position - initialPoint.position, -transform.up)), 0, toothLength);
@@ -34,14 +39,13 @@ public class Tooth : MonoBehaviour
             {
                 highestDistance = distanceFromInitial;
                 float lerpValue = distanceFromInitial / toothLength;
-                GetComponent<Renderer>().material.SetVector("_Offset", new Vector2(0, Mathf.Clamp01(lerpValue - offset)));
+                foreach (int x in materialNumbers) avatarMesh.materials[x].SetVector("_Offset", new Vector2(0, Mathf.Clamp01(lerpValue - offset)));
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("touchy");
         if (other.GetComponent<Biteable>())
         {
             if (initialPoint != null)
@@ -50,24 +54,30 @@ public class Tooth : MonoBehaviour
 
                 if (distanceBetweenInitials <= maxDistanceBetweenInitialPoints) //Checks if close to old bite
                 {
+                    Debug.Log("Too close");
                     bite = true;
                 }
                 else
                 {
+                    Debug.Log("Chomp 2");
                     bite = true;
                     string gameObjectName = new string("Initial " + other.gameObject);
                     initialPoint = new GameObject(gameObjectName).transform;
                     initialPoint.position = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(toothTip.position);
                     initialPoint.parent = other.transform;
                     bloodColour = other.GetComponent<Biteable>().bloodColour;
-                    if (bloodColour != GetComponent<Renderer>().material.GetColor("_BloodColour"))
+                    
+                    foreach (int x in materialNumbers)
                     {
-                        highestDistance = 0;
-                        float lerpValue = highestDistance / toothLength;
-                        GetComponent<Renderer>().material.SetVector("_Offset", new Vector2(0, Mathf.Clamp01(lerpValue - offset)));
+                        if (bloodColour != avatarMesh.materials[x].GetColor("_BloodColour"))
+                        {
+                            highestDistance = 0;
+                            float lerpValue = highestDistance / toothLength;
+                            avatarMesh.materials[x].SetVector("_Offset", new Vector2(0, Mathf.Clamp01(lerpValue - offset)));
+                        }
+                        avatarMesh.materials[x].SetColor("_BloodColour", bloodColour);
+                        avatarMesh.materials[x].SetColor("_ToothColour", toothColour);
                     }
-                    GetComponent<Renderer>().material.SetColor("_BloodColour", bloodColour);
-                    GetComponent<Renderer>().material.SetColor("_ToothColour", toothColour);
 
                     //Below is the creation of the decals
                     GameObject decalHole = Instantiate(holeDecal); //Might need to replace with decal projector
@@ -75,6 +85,7 @@ public class Tooth : MonoBehaviour
                     decalHole.transform.position = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(toothTip.position);
                     decalHole.GetComponent<EasyDecal>().DecalMaterial.color = bloodColour;
                     decalHole.GetComponent<EasyDecal>().ProjectionTarget = other.GetComponent<Biteable>().decalTarget;
+                    decalHole.GetComponent<EasyDecal>().Distance = 0.1f;
                     //Sizing and stuff
                 }
             }
@@ -88,8 +99,11 @@ public class Tooth : MonoBehaviour
                 initialPoint.position = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(toothTip.position);
                 initialPoint.parent = other.transform;
                 bloodColour = other.GetComponent<Biteable>().bloodColour;
-                GetComponent<Renderer>().material.SetColor("_BloodColour", bloodColour);
-                GetComponent<Renderer>().material.SetColor("_ToothColour", toothColour);
+                foreach (int x in materialNumbers)
+                {
+                    avatarMesh.materials[x].SetColor("_BloodColour", bloodColour);
+                    avatarMesh.materials[x].SetColor("_ToothColour", toothColour);
+                }
 
                 //Below is the creation of the decals
                 GameObject decalHole = Instantiate(holeDecal);
@@ -97,6 +111,7 @@ public class Tooth : MonoBehaviour
                 decalHole.transform.position = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(toothTip.position);
                 decalHole.GetComponent<EasyDecal>().DecalMaterial.color = bloodColour;
                 decalHole.GetComponent<EasyDecal>().ProjectionTarget = other.GetComponent<Biteable>().decalTarget;
+                decalHole.GetComponent<EasyDecal>().Distance = 0.1f;
                 //Sizing and stuff
             }
         }
